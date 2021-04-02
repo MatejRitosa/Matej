ea80d7fef79070a04fc6089ac930cdf79a5bf015
Matej <ritossamatej@gmail.com>
2021-04-02 15:02:00 +0200

    Novi projekt

---
+++ intelij-demo/src/test/java/com/example/intelijdemo/IntelijDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intelijdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelijDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
